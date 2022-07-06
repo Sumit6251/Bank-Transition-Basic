d346b81f45d502887fa7d75ccaa4a0c98aa7d6da
Sumit <sumitbiswas6251@gmail.com>
2022-07-06 13:32:38 +0600

    All Done!

--- js/banking.js
+++ js/banking.js
@@ -1,25 +1,49 @@
-document.querySelector('.depositBtn').addEventListener('click',function(){
-
-    let depositeInput = document.querySelector('.depositInput');
+document.querySelector('.depositBtn').addEventListener('click', function(){
+    let depositInput = document.querySelector('.depositInput');
+    let depositAmount = depositInput.value;
+    let depositAmountNum = parseFloat(depositAmount);
     
-    let depositTotal = document.querySelector('.depositTotal');
-    let depositeAmountText = depositeInput.value;
-    let depositeAmount = parseFloat(depositeAmountText);
+    let deposittotal = document.querySelector('.depositTotal');
+    let depositTotalText = deposittotal.innerText;
+    let depositTotalNum = parseFloat(depositTotalText);
 
-    let currentDepositText = depositTotal.innerHTML;
-    let currentDeposit = parseFloat(currentDepositText);
-    let newDepositTotal =currentDeposit + depositeAmount;
-    depositTotal.innerHTML = newDepositTotal;
-     
-    let balanceTotal = document.querySelector('.balanceTotal').innerHTML;
-    let balanceTotalText = balanceTotal.innerText
-    let balance = parseFloat(balanceTotalText);
+    let depositTotal = depositAmountNum + depositTotalNum;
+    deposittotal.innerText = depositTotal;
     
-    let Totalbalance = balanceTotal + newDepositTotal;
+    let balanceTotal = document.querySelector('.balanceTotal');
+    let balanceTotalText = balanceTotal.innerText;
+    let balanceTotalNum = parseFloat(balanceTotalText);
 
-    balanceTotal.innerHTML = Totalbalance;
+    balanceTotal.innerText = balanceTotalNum + depositAmountNum;
+
+
+    depositInput.value = ''
+})
+
+document.querySelector('.withdrawbtn').addEventListener('click', function(){
+    let withdrawInput = document.querySelector('.withdrawInput');
+    let withdrawInputText = withdrawInput.value;
+    let withdrawInputNum = parseFloat(withdrawInputText);
 
 
 
-    depositeInput.value = ''
+    let withdrawTotal = document.querySelector('.withdrawTotal');
+    let withdrawTotalText = withdrawTotal.innerText;
+    let withdrawTotalNum = parseFloat(withdrawTotalText);
+
+    let finalWithdrawTotal = withdrawInputNum + withdrawTotalNum;
+
+    withdrawTotal.innerText = finalWithdrawTotal;
+
+    withdrawInput.value = ''
+
+
+
+    let balanceTotal = document.querySelector('.balanceTotal');
+    let balanceTotalText = balanceTotal.innerText;
+    let balanceTotalNum = parseFloat(balanceTotalText);
+
+    balanceTotal.innerText = balanceTotalNum - withdrawInputNum;
+
+    
 })
